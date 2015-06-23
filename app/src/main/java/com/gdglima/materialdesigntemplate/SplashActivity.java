b84ed7646f596acc483a248e3a5e9ba585f69e73
AlvarDev <deivid07.21@gmail.com>
2015-06-23 01:24:41 -0500

    gradle

--- app/src/main/java/com/gdglima/materialdesigntemplate/SplashActivity.java
+++ app/src/main/java/com/gdglima/materialdesigntemplate/SplashActivity.java
@@ -1,12 +1,10 @@
 package com.gdglima.materialdesigntemplate;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 
-public class SplashActivity extends ActionBarActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +12,5 @@ public class SplashActivity extends ActionBarActivity {
         setContentView(R.layout.activity_splash);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
